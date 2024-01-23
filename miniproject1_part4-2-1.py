7fd9ad204b168fb4b6101a075ecf9c1201bb3198
gaurav010101 <55657874+gaurav010101@users.noreply.github.com>
2024-01-23 10:00:01 -0800

    Update miniproject1_part4-2-1.py

--- miniproject1_part4-2-1.py
+++ miniproject1_part4-2-1.py
@@ -53,8 +53,8 @@ def get_model_id_gdrive(model_type):
         word_index_id = "13qMXs3-oB9C6kfSRMwbAtzda9xuAUtt8"
         embeddings_id = "1-RXcfBvWyE-Av3ZHLcyJVsps0RYRRr_2"
     elif model_type == "50d":
-        embeddings_id = "1DBaVpJsitQ1qxtUvV1Kz7ThDc3az16kZ"
-        word_index_id = "1rB4ksHyHZ9skes-fJHMa2Z8J1Qa7awQ9"
+        embeddings_id = "1J-mbvwP_XiNdhzIMjufW7-NW1hueU5CC"
+        word_index_id = "1F0i_wCuTI2M632VCV-Oa2xM7rZiTC08X"
     elif model_type == "100d":
         word_index_id = "1-oWV0LqG3fmrozRZ7WB1jzeTJHRUI3mq"
         embeddings_id = "1SRHfX130_6Znz7zbdfqboKosz-PfNvNp"
